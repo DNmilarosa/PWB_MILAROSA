b63261dbeb4c4b0fd2beeb152fb1e4dd2936b690
Mila Rosa <milarosa@smkwikrama.sch.id>
2022-02-06 11:51:14 +0700

    MVC - Tugas 5

---
+++ PHPMVC/app/core/Constants.php
@@ -0,0 +1,2 @@
+<?php
+define('BASEURL', 'http://localhost//PWB_MILAROSA/PHPMVC/public/');
---
+++ PHPMVC/app/views/about/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Halaman About</title>
+</head>
+<body>
+<div class="container">
+<h1 class="mt-3">About Me</h1>
+<img src="<?= BASEURL; ?>/img/profile.gif" alt="Fajar Septian" width="200" clas
+s="mb-3 rounded-circle shadow">
+<p>Halo nama saya <?= $data['nama']; ?>, dan pekerjaan saya adalah <?= $data['p
+ekerjaan']; ?>.</p>
+</div>
+</body>
+</html>
